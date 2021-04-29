cba0b9a98aaff122bf90c963726fe8d397bb40f0
jessicap5159 <jessicaj5159@gmail.com>
2021-04-28 17:50:20 -0700

    product routes created, still need to troubleshoot a couple errors

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
+const productRoutes = require('./api/product-routes');
 
 router.use('/api', apiRoutes);
+router.use('/products', productRoutes);
 
 router.use((req, res) => {
   res.send("<h1>Wrong Route!</h1>")
